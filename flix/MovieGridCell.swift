d45bc7b366797de7b68bf414311a06eff25cdb7b
MCMitchell8 <melissacmitchell8@gmail.com>
2020-10-02 22:28:22 -0700

    More improvements

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Melissa C. Mitchell on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
